36a9657cdd46ac07e548b638be9a68ce2ad286b6
Saravana raju R <88960950+SaravanarajuR@users.noreply.github.com>
2023-02-01 15:04:42 +0530

    Josephu's problem

---
+++ Codes/Queue/josephus problem.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node *next;
+};
+struct node *head = 0, *newnode, *tail = 0, *temp, *del;
+
+void find(struct node *head, int k) {
+  temp = head;
+  for (int i = 1; i < k; i++) {
+    temp = temp->next;
+  }
+  del = temp->next;
+  temp->next = temp->next->next;
+  free(del);
+  while (temp->next == temp) {
+    printf("%d", temp->data);
+    return;
+  }
+  find(temp, k);
+}
+
+void main() {
+  int n, d, k, l;
+  printf("\nEnter the number of nodes");
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    printf("\nEnter the data");
+    scanf("%d", &d);
+    newnode = malloc(sizeof(struct node));
+    newnode->data = d;
+    newnode->next = 0;
+    if (head == NULL) {
+      head = tail = newnode;
+    } else {
+      newnode->next = head;
+      tail->next = newnode;
+      tail = newnode;
+    }
+  }
+  printf("Enter the k value");
+  scanf("%d", &k);
+  find(head, k);
+}
