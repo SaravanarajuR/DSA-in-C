ec414792a1b0401af48419b81f7827167d2482c5
Saravana raju R <88960950+SaravanarajuR@users.noreply.github.com>
2023-02-06 22:24:10 +0530

    Update josephus problem.c

--- Codes/Queue/josephus problem.c
+++ Codes/Queue/josephus problem.c
@@ -1,46 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct node {
-  int data;
-  struct node *next;
-};
-struct node *head = 0, *newnode, *tail = 0, *temp, *del;
-
-void find(struct node *start, int k, int i) {
-  temp = start;
-  for (int i; i < k; i++) {
-    temp = temp->next;
-  }
-  del = temp->next;
-  temp->next = temp->next->next;
-  free(del);
-  while (temp->next == temp) {
-    printf("%d", temp->data);
-    return;
-  }
-  find(temp, k, 1);
-}
-
-void main() {
-  int n, d, k, l;
-  printf("\nEnter the number of nodes");
-  scanf("%d", &n);
-  for (int i = 0; i < n; i++) {
-    printf("\nEnter the data");
-    scanf("%d", &d);
-    newnode = malloc(sizeof(struct node));
-    newnode->data = d;
-    newnode->next = 0;
-    if (head == NULL) {
-      head = tail = newnode;
-    } else {
-      newnode->next = head;
-      tail->next = newnode;
-      tail = newnode;
-    }
-  }
-  printf("Enter the k value");
-  scanf("%d", &k);
-  find(head, k, 2);
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int playerid;
+	struct node *next;
+}*front=0,*newnode,*temp;
+int main()
+{
+	int i,j,n,k,count=0;
+	printf("\nEnter the no. of Players: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		newnode=(struct node*)malloc(sizeof(struct node));
+		newnode->playerid=i;
+		if(front==0)
+			front=temp=newnode;
+		else
+		{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		newnode->next=front;
+	}
+	printf("Players Ready");
+	printf("\n\nEnter the no. to eliminate: ");
+	scanf("%d",&k);
+	for(j=n;j>1;j--)
+	{
+		count++;
+		for(i=1;i<k;i++)
+			temp=temp->next;
+		printf("\nPlayer no. %d Eliminated in round %d",temp->next->playerid,count);
+		temp->next=temp->next->next;
+	}
+	printf("\nWinner is %d",temp->playerid);
 }
