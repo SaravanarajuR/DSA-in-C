00e15d48324031c33c3226539ecf415015dd84a0
Saravana raju R <88960950+SaravanarajuR@users.noreply.github.com>
2023-02-06 22:27:36 +0530

    Update josephus problem.c

--- Codes/Queue/josephus problem.c
+++ Codes/Queue/josephus problem.c
@@ -5,7 +5,7 @@ struct node
 	int playerid;
 	struct node *next;
 }*front=0,*newnode,*temp;
-int main()
+void main()
 {
 	int i,j,n,k,count=0;
 	printf("\nEnter the no. of Players: ");
@@ -26,13 +26,16 @@ int main()
 	printf("Players Ready");
 	printf("\n\nEnter the no. to eliminate: ");
 	scanf("%d",&k);
-	for(j=n;j>1;j--)
+	for(j=n;j>1;j--) //To track no of players alive
 	{
 		count++;
-		for(i=1;i<k;i++)
+		for(i=1;i<k;i++){
 			temp=temp->next;
+		}
 		printf("\nPlayer no. %d Eliminated in round %d",temp->next->playerid,count);
+		del=temp->next;
 		temp->next=temp->next->next;
+		free(del);
 	}
 	printf("\nWinner is %d",temp->playerid);
 }
