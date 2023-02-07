7152f82f5488ba124eaa74e738fe11bc26f3791b
Saravana raju R <88960950+SaravanarajuR@users.noreply.github.com>
2023-02-08 00:58:35 +0530

    Create postfix-eval.c

---
+++ Codes/stack/postfix-eval.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(int item) {
+    stack[++top] = item;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int eval(int op1, int op2, char operator) {
+    switch (operator) {
+        case '+': return op2 + op1;
+        case '-': return op2 - op1;
+        case '*': return op2 * op1;
+        case '/': return op2 / op1;
+        case '^': return pow(op2, op1);
+    }
+    return 0;
+}
+
+int evaluatePostfix(char postfix[]) {
+    int i, op1, op2;
+    char ch;
+
+    for (i = 0; i < strlen(postfix); i++) {
+        ch = postfix[i];
+        if (isdigit(ch)) {
+            push(ch - '0');
+        } else {
+            op1 = pop();
+            op2 = pop();
+            push(eval(op1, op2, ch));
+        }
+    }
+    return stack[top];
+}
+
+int main() {
+    char postfix[MAX_SIZE];
+    printf("Enter a postfix expression: ");
+    scanf("%s", postfix);
+    printf("Result: %d\n", evaluatePostfix(postfix));
+    return 0;
+}
